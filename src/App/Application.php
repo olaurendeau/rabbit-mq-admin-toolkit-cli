f6ca38126a47c6da0223ef36da0ce7b5d672710a
Olivier Laurendeau <o.laurendeau@gmail.com>
2016-03-29 07:46:50 +0200

    Initialize CLI

---
+++ src/App/Application.php
@@ -0,0 +1,91 @@
+<?php
+namespace App;
+
+use App\Lib\ErrorHandler;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class Application extends \Symfony\Component\Console\Application
+{
+    /**
+     * @var ContainerBuilder
+     */
+    protected $container;
+
+    public function getContainer()
+    {
+        return $this->container;
+    }
+
+    /**
+     * @param string $baseNamespaceName Highest level namespace for the application
+     */
+    public function __construct($baseNamespaceName)
+    {
+        // our error handler
+        ErrorHandler::register();
+
+        // create and populate the container
+        $this->container = new ContainerBuilder();
+
+        // some useful paths
+        $paths = array();
+        $paths['root'] = __DIR__ . '/../../';
+        $paths['config'] = $paths['root'] . 'app/config/';
+        $paths['build'] = $paths['root'] . 'build/';
+        $this->container->setParameter('paths', $paths);
+
+        // the main config
+        $loader = new YamlFileLoader($this->container, new FileLocator($paths['config']));
+        $loader->load('config.yml');
+        $loader->load('services.yml');
+
+        // construct the application
+        $app = $this->container->getParameter('application');
+        $version = $this->container->getParameter('version');
+        parent::__construct($app['name'],$version['current']);
+
+        // and add commands to it
+        $this->addConsoleCommands($baseNamespaceName);
+
+        // may be we have some commands also
+        $this->addConsoleCommands(__NAMESPACE__);
+    }
+
+    /**
+     * Adds all existing console commands
+     *
+     * @param string $baseNamespaceName Base namespace for the commands
+     */
+    protected function addConsoleCommands($baseNamespaceName)
+    {
+        // get all namespaces from the composer autoload list
+        $paths = $this->container->getParameter('paths');
+        $namespaces = include $paths['root'] . '/vendor/composer/autoload_namespaces.php';
+        // find the path for the namespace
+        foreach ($namespaces as $namespace => $lookupPaths) {
+            if ($namespace == $baseNamespaceName) {
+                // add all existing commands
+                foreach ($lookupPaths as $path) {
+                    $commandPath = $path . '/' . $namespace . '/Command/';
+                    if (is_dir($commandPath)) {
+                        $files = Finder::create()->files()->name('*Command.php')->in($path . '/' . $namespace . '/Command/');
+                        foreach ($files as $file) {
+                            $className = $file->getBasename('.php'); // strip .php extension
+                            $r = new \ReflectionClass($baseNamespaceName . '\Command' . '\\' . $className);
+                            if (!$r->isAbstract()) {
+                                $command = $r->newInstance();
+                                $command->setContainer($this->container);
+
+                                $this->add($command);
+                            }
+                        }
+                    }
+                }
+                break;
+            }
+        }
+    }
+}
---
+++ src/App/Command/DefineCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Command;
+
+use Ola\RabbitMqAdminToolkitBundle\Command\VhostDefineCommand;
+use Ola\RabbitMqAdminToolkitBundle\DependencyInjection\OlaRabbitMqAdminToolkitExtension;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Parser;
+
+class DefineCommand extends VhostDefineCommand
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function configure()
+    {
+        $this->addArgument('config-file', InputArgument::OPTIONAL, "Path to the config file to use");
+
+        parent::configure();
+
+        $this->setName('define');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->loadConfiguration($input);
+
+        parent::execute($input, $output);
+    }
+
+    protected function loadConfiguration(InputInterface $input)
+    {
+        if (null === $input->getArgument('config-file')) {
+            $configFile = '.rabbit-mq-admin-toolkit.yml';
+        } else {
+            $configFile = $input->getArgument('config-file');
+        }
+
+        if (!file_exists($configFile)) {
+            throw new \InvalidArgumentException(sprintf('Unable to find config file at path "%s"', $configFile));
+        }
+
+        $extension = new OlaRabbitMqAdminToolkitExtension();
+
+        $container = $this->getContainer();
+        $parser = new Parser();
+        $config = $parser->parse(file_get_contents($configFile));
+        $extension->load(array($config), $container);
+    }
+}
